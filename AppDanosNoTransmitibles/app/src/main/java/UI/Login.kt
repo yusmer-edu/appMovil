b56bdd63c408150179514e56ad37fe1631d83791
yussmer <yusmercayetano@gmail.com>
2025-02-27 05:46:25 -0500

    avance de app de daños

--- AppDanosNoTransmitibles/app/src/main/java/UI/Login.kt
+++ AppDanosNoTransmitibles/app/src/main/java/UI/Login.kt
@@ -1,12 +1,17 @@
 package UI
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.WindowInsetsController
 import android.widget.Button
 import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import com.softyus.appdanosnotransmitibles.R
 
@@ -25,6 +30,24 @@ class Login : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            // Para Android 11 (API 30) y superior
+            window.insetsController?.setSystemBarsAppearance(
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+            )
+        } else {
+            // Para versiones anteriores a Android 11
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+
+        // Asegurar que el contenido no se superponga con la barra de estado
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        // Cambiar el color de la barra de estado
+        window.statusBarColor = Color.WHITE
+
 
         userName = findViewById(R.id.user_name)
         password = findViewById(R.id.password_input)
--- AppDanosNoTransmitibles/app/src/main/java/UI/Options.kt
+++ AppDanosNoTransmitibles/app/src/main/java/UI/Options.kt
@@ -1,9 +1,14 @@
 package UI
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsetsController
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import com.softyus.appdanosnotransmitibles.R
 
@@ -17,5 +22,23 @@ class Options : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            // Para Android 11 (API 30) y superior
+            window.insetsController?.setSystemBarsAppearance(
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+            )
+        } else {
+            // Para versiones anteriores a Android 11
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+
+        // Asegurar que el contenido no se superponga con la barra de estado
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        // Cambiar el color de la barra de estado
+        window.statusBarColor = Color.WHITE
+
     }
 }
